0d8f2e7746992fcccb2f80291d2c72496e3392da
Nikita Petrovets <lmikon007@gmail.com>
2025-05-31 23:43:36 +0300

    feat: add config, swagger and implement base project structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { readFile } from 'fs/promises';
+import { SwaggerModule } from '@nestjs/swagger';
+import { resolve } from 'path';
+import { parse } from 'yaml';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  const swaggerPath = resolve(__dirname, '..', 'doc', 'api.yaml');
+  const swaggerFile = await readFile(swaggerPath, 'utf-8');
+  SwaggerModule.setup('doc', app, parse(swaggerFile));
+
+  const configService = app.get(ConfigService);
+  let port = parseInt(configService.get('PORT'), 10);
+  const alternativePort = 4000;
+  if (!port) {
+    port = alternativePort;
+    console.error(
+      `Cannot start app with the port from .env file.
+      Port ${port} was used`,
+    );
+  }
+  console.log('Starting app on port: ' + port);
+  await app.listen(port);
 }
 bootstrap();
