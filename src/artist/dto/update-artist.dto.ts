41dea369ba7c75d294a99f0044691e35b7abde48
Nikita Petrovets <lmikon007@gmail.com>
2025-06-02 20:33:14 +0300

    fix: fix artist dto

--- src/artist/dto/update-artist.dto.ts
+++ src/artist/dto/update-artist.dto.ts
@@ -1,10 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
 
 export class UpdateArtistDto {
+  @ApiProperty({
+    example: 'Bob Marley',
+    description: 'name of the artist',
+  })
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({ example: true, description: 'does artist have grammy' })
   @IsBoolean()
   @IsNotEmpty()
   grammy: boolean;
