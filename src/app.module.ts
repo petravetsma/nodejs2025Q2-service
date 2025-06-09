af959cecb79b3443e8ca31301e176392efd122bf
Nikita Petrovets <lmikon007@gmail.com>
2025-06-10 00:05:36 +0300

    feat: implement db migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,35 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AlbumModule } from 'src/album/album.module';
 import { ArtistModule } from 'src/artist/artist.module';
+import { FavoritesModule } from 'src/favorites/favorites.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TrackModule } from './track/track.module';
 import { UserModule } from './user/user.module';
-import { FavoritesModule } from 'src/favorites/favorites.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { Album } from 'src/album/entities/album.entity';
+import { Artist } from 'src/artist/entities/artist.entity';
+import { Favorites } from 'src/favorites/entities/favorites.entity';
+import { Track } from 'src/track/entities/track.entity';
+import { User } from 'src/user/entities/user.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        type: 'postgres',
-        host: config.get('POSTGRES_HOST'),
-        port: +config.get('POSTGRES_PORT'),
-        username: config.get('POSTGRES_USER'),
-        password: config.get('POSTGRES_PASSWORD'),
-        database: config.get('POSTGRES_DB'),
-        autoLoadEntities: true,
-        synchronize: true, // only for dev
-      }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT || '5432'),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [User, Artist, Album, Track, Favorites],
+      synchronize: false,
+      migrations: ['dist/src/migrations/*.js'],
+      migrationsRun: true,
     }),
+    TypeOrmModule.forFeature([User, Artist, Album, Track, Favorites]),
     UserModule,
     TrackModule,
     ArtistModule,
